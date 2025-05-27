d8b5b8f8ec4f976afb30015cec65ba9b8af9215b
thakur7071 <singhnaveen2370@gmail.com>
2025-05-27 10:28:52 +0530

    updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,15 +117,7 @@ function Navbar() {
                 </Link>
               ))}
 
-              {/* Login Button in Mobile Menu */}
-              <Link
-                to="/login"
-                onClick={() => setIsMenuOpen(false)}
-                className="w-fit bg-blue-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-blue-700 transition"
-              >
-                <FaSignInAlt />
-                Login
-              </Link>
+             
             </nav>
           </div>
         )}
